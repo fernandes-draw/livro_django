4fc187675a1c2338a491d48a5891bd034a62a15b
fernandes-draw <fernandes_draw@hotmail.com>
2026-03-05 16:21:35 -0300

    Customização avançada

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -4,15 +4,43 @@ from .models import *
 
 class ProfileAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
-    list_display = ('user', 'role', 'birthday',)
+    list_display = ('user', 'specialtiesList', 'addressesList',)
     empty_value_display = 'Vazio'
-    list_display_links = ('user', 'role',)
+    # list_display_links = ('user', 'role',)
     list_filter = ('user__is_active',)
-    fields = ('user', ('role', ), 'image',
-              'birthday', 'specialties', 'addresses',)
+    fieldsets = (
+        ('Usuário', {
+            'fields': ('user', 'birthday', 'image')
+        }),
+        ('Função', {
+            'fields': ('role',)
+        }),
+        ('Extras', {
+            'fields': ('specialties', 'addresses')
+        }),
+    )
     exclude = ('favorites', 'created_at', 'updated_at',)
     readonly_fields = ('user',)
     search_fields = ('user__username',)
+
+    def birth(self, obj):
+        if obj.birthday:
+            return obj.birthday.strftime("%d/%m/%Y")
+
+    birth.empty_value_display = '___/___/_____'
+
+    def specialtiesList(self, obj):
+        return [i.name for i in obj.specialties.all()]
+
+    def addressesList(self, obj):
+        return [i.name for i in obj.addresses.all()]
+
+    class Media:
+        css = {
+            "all": ("css/custom.css",)
+
+        }
+        js = ("js/custom.js",)
 
 
 admin.site.register(Profile, ProfileAdmin)
