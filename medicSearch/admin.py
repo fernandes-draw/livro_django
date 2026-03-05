325350220c6654cd87052054668c1287a2ba76e0
fernandes-draw <fernandes_draw@hotmail.com>
2026-03-05 12:49:27 -0300

    Customizando o admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('user', 'role', 'birthday',)
+    empty_value_display = 'Vazio'
+    list_display_links = ('user', 'role',)
+    list_filter = ('user__is_active',)
+    fields = ('user', ('role', ), 'image',
+              'birthday', 'specialties', 'addresses',)
+    exclude = ('favorites', 'created_at', 'updated_at',)
+    readonly_fields = ('user',)
+    search_fields = ('user__username',)
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
