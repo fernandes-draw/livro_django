df56c1ec810163889f4d1b902d4a57493c0bb630
Everson Fernandes <fernandes_draw@hotmail.com>
2026-03-05 20:37:10 -0300

    Customizando urls no Django

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -8,6 +8,7 @@ urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("", include("medicSearch.urls.HomeUrls")),
+        path("profile/", include("medicSearch.urls.ProfileUrls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
